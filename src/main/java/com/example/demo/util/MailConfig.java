3ca98b1fafd92f1187e187b8910003d85f00e5c6
bouchouirab ibtissam <43052266+bouchouirabI@users.noreply.github.com>
2020-10-12 15:44:23 +0100

    Update MailConfig.java

--- src/main/java/com/example/demo/util/MailConfig.java
+++ src/main/java/com/example/demo/util/MailConfig.java
@@ -17,8 +17,8 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("bouchouirab.ibi@gmail.com");
-        mailSender.setPassword("ibi@2003");
+        mailSender.setUsername("pour.git.hub@gmail.com");
+        mailSender.setPassword("gitHub@20");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
